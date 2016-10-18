c3391e694a0349b07aecbf184b15dff446ebcaa7
Ali Momeni <batchku@gmail.com>
2016-10-18 17:08:41 -0400

    initial upload

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -12,7 +12,48 @@ using namespace cv;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    
+    TRAIN_MODEL = 0;           //1 if you are training the models, 0 if you are running the program to predict
+    TEST_MODEL  = 1;           //0 if you are training the models, 1 if you are running the program to predict
+    
+    target_width = 320;			// for resizing the input (small is faster)
+    
+    // maximum number of image masks that you will use
+    // must have the masks prepared in advance
+    // only used at training time
+    num_models_to_train = 16;
+    
+    
+    // number of models used to compute a single pixel response
+    // must be less than the number of training models
+    // only used at test time
+    num_models_to_average = 10;
+    
+    // runs detector on every 'step_size' pixels
+    // only used at test time
+    // bigger means faster but you lose resolution
+    // you need post-processing to get contours
+    step_size = 3;
+    
+    // Assumes a certain file structure e.g., /root/img/basename/00000000.jpg
+    root = "/Users/ali/Documents/Development/of_v0.9.4_osx_release/apps/myApps/handtrack/";       //replace with path to your Xcode project
+    basename = "";
+    img_prefix		= root + "img"		+ basename + "/";			// color images
+    msk_prefix		= root + "mask"     + basename + "/";			// binary masks
+    model_prefix		= root + "models"	+ basename + "/";			// output path for learned models
+    globfeat_prefix  = root + "globfeat" + basename + "/";			// output path for color histograms
+    
+    // types of features to use (you will over-fit if you do not have enough data)
+    // r: RGB (5x5 patch)
+    // v: HSV
+    // l: LAB
+    // b: BRIEF descriptor
+    // o: ORB descriptor
+    // s: SIFT descriptor
+    // u: SURF descriptor
+    // h: HOG descriptor
+    string feature_set = "rvl";
+    
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -22,35 +22,35 @@ class ofApp : public ofBaseApp{
 		void gotMessage(ofMessage msg);
     
     
-        bool TRAIN_MODEL = 0;           //1 if you are training the models, 0 if you are running the program to predict
-        bool TEST_MODEL  = 1;           //0 if you are training the models, 1 if you are running the program to predict
+        bool TRAIN_MODEL;           //1 if you are training the models, 0 if you are running the program to predict
+        bool TEST_MODEL;           //0 if you are training the models, 1 if you are running the program to predict
 
-        int target_width = 320;			// for resizing the input (small is faster)
+        int target_width;			// for resizing the input (small is faster)
         
         // maximum number of image masks that you will use
         // must have the masks prepared in advance
         // only used at training time
-        int num_models_to_train = 16;
+        int num_models_to_train;
         
         
         // number of models used to compute a single pixel response
         // must be less than the number of training models
         // only used at test time
-        int num_models_to_average = 10;
+        int num_models_to_average;
         
         // runs detector on every 'step_size' pixels
         // only used at test time
         // bigger means faster but you lose resolution
         // you need post-processing to get contours
-        int step_size = 3;
+        int step_size;
         
         // Assumes a certain file structure e.g., /root/img/basename/00000000.jpg
-        string root = "/Users/ali/Documents/Development/handtrack/";       //replace with path to your Xcode project
-        string basename = "";
-        string img_prefix		= root + "img"		+ basename + "/";			// color images
-        string msk_prefix		= root + "mask"     + basename + "/";			// binary masks
-        string model_prefix		= root + "models"	+ basename + "/";			// output path for learned models
-        string globfeat_prefix  = root + "globfeat" + basename + "/";			// output path for color histograms
+        string root;                        //replace with path to your Xcode project
+        string basename;
+        string img_prefix;                  // color images
+        string msk_prefix;                  // binary masks
+        string model_prefix;                // output path for learned models
+        string globfeat_prefix;  			// output path for color histograms
     
         // types of features to use (you will over-fit if you do not have enough data)
         // r: RGB (5x5 patch)
@@ -61,6 +61,6 @@ class ofApp : public ofBaseApp{
         // s: SIFT descriptor
         // u: SURF descriptor
         // h: HOG descriptor
-        string feature_set = "rvl";
+        string feature_set;
     
 };
